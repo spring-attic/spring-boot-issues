b70187f364d713ba0026f094a97eed80624f009d
Phillip Webb <pwebb@gopivotal.com>
2014-05-16 10:43:38 +0100

    gh-886

---
+++ gh-886/src/main/java/multimain/Main1.java
@@ -0,0 +1,10 @@
+package multimain;
+
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		System.out.println("Main1");
+	}
+
+}
---
+++ gh-886/src/main/java/multimain/Main2.java
@@ -0,0 +1,10 @@
+package multimain;
+
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		System.out.println("Main2");
+	}
+
+}
