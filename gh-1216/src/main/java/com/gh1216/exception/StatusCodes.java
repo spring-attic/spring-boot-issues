0c2ce9b35fb90628026f3921694daea0e6ab5c2c
Buddhavarapu <sbuddhavarapu@localhost>
2014-07-07 16:40:15 -0700

    initial commit

---
+++ gh-1216/src/main/java/com/gh1216/exception/StatusCodes.java
@@ -0,0 +1,8 @@
+package com.gh1216.exception;
+
+public class StatusCodes {
+    public static final String UNKNOWN_SYSTEM_ERROR = "IAM_1000";
+    public static final String DATA_VALIDATION_ERROR = "IAM_2000";
+    public static final String DATA_INTEGRITY_ERROR = "IAM_3000";
+    public static final String TRANSACTION_SYSTEM_ERROR = "IAM_4000";
+}
