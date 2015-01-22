4bdd8951b2dffd8eb3e7fd4382b5409bc4960675
Chris Savory <csavory@gmail.com>
2015-01-22 11:40:48 -0700

    Add a project that reproduces gh-2398

--- gh-2398/src/main/java/com/edlogics/elrc/controller/IndexController.java
+++ gh-2398/src/main/java/com/edlogics/elrc/controller/IndexController.java
@@ -16,13 +16,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.edlogics.elrc.controller.model.FakeObject;
 
 @Controller
 @RequestMapping("/")
@@ -39,7 +34,7 @@ public class IndexController {
 	 * @return The view name (an HTML page with Thymeleaf markup).
 	 */
 	@RequestMapping({ "/", "/home" })
-	public @ResponseBody ResponseEntity<FakeObject> home( HttpServletRequest request, HttpServletResponse response, Locale locale ) {
-		return new ResponseEntity<>( new FakeObject(), HttpStatus.OK );
+	public String home( HttpServletRequest request, HttpServletResponse response, Locale locale ) {
+		return "index";
 	}
 }
