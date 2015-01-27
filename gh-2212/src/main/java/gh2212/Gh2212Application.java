16817cb8b61b47e56b5455ec31297f438b28b12b
Andy Wilkinson <awilkinson@pivotal.io>
2015-01-27 12:44:45 +0000

    Project to reproduce issue 2212

---
+++ gh-2212/src/main/java/gh2212/Gh2212Application.java
@@ -0,0 +1,13 @@
+package gh2212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gh2212Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gh2212Application.class, args);
+	}
+
+}
