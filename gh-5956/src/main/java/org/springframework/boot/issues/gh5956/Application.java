8cc2b79697ffe733707eb770f3a54b90e001169e
Mihhail Verhovtsov <mihhail.verhovtsov@icefire.ee>
2016-05-13 15:05:07 +0300

    gh-5956

---
+++ gh-5956/src/main/java/org/springframework/boot/issues/gh5956/Application.java
@@ -0,0 +1,12 @@
+package org.springframework.boot.issues.gh5956;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
