3e16fcb533b79e0469e29a89b8d069e44fa8c797
Dave Syer <dsyer@pivotal.io>
2016-10-14 10:57:43 +0100

    Update sample to 'legacy' status

--- gh-7161/src/main/java/com/example/SimpleApplication.java
+++ gh-7161/src/main/java/com/example/SimpleApplication.java
@@ -2,18 +2,28 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SimpleApplication {
 
 	public static void main(String[] args) throws IOException {
 		Resource resource = new ClassPathResource("/mydir");
-		Paths.get(resource.getURI());
+		// Paths.get(resource.getURI());
+		URI uri = resource.getURI();
+		System.out.println(uri);
+		FileSystems.newFileSystem(uri, Collections.emptyMap());
+		Path path = Paths.get(uri);
 		SpringApplication.run(SimpleApplication.class, args);
 	}
 }
