f9e8672081f509070e5c905dd348ab4ce0c693d8
Janne Valkealahti <janne.valkealahti@gmail.com>
2017-03-10 18:45:44 +0000

    Issue project for gh-8569

---
+++ gh-8569/src/main/java/com/example/MyConfigurationProperties.java
@@ -0,0 +1,7 @@
+package com.example;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "my.properties")
+public class MyConfigurationProperties {
+}
