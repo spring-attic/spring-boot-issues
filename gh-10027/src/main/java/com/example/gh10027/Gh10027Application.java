ce770d7b234dad7f97670303cd8b4a32c83f80aa
Stephane Nicoll <snicoll@pivotal.io>
2017-08-17 10:20:51 +0200

    Add repro project for gh-10027

---
+++ gh-10027/src/main/java/com/example/gh10027/Gh10027Application.java
@@ -0,0 +1,12 @@
+package com.example.gh10027;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gh10027Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gh10027Application.class, args);
+	}
+}
