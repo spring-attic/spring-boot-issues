7ac0ec15f9dfc4487ff58f39c912fa3bc41c2c9e
Nena Raab <nena.raab@sap.com>
2017-11-30 19:17:30 +0100

    DataJpaTest fails when updating to SpringBoot 1.5.9.RELEASE https://github.com/spring-projects/spring-boot/issues/10465

---
+++ gh-10465-2/src/test/java/org/springframework/boot/issues/gh10465/TestConfig.java
@@ -0,0 +1,10 @@
+package org.springframework.boot.issues.gh10465;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+
+//@SpringBootConfiguration
+//@AutoConfigurationPackage
+//public class TestConfig {
+//   
+//}
