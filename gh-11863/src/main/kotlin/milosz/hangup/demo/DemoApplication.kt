ecda5c69d289315d33272036987d173026e7aa70
Miłosz Tylenda <milosz.tylenda@empik.com>
2018-01-31 13:07:12 +0100

    A test case for github issue 11863

---
+++ gh-11863/src/main/kotlin/milosz/hangup/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package milosz.hangup.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
